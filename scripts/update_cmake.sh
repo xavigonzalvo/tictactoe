050ddc6140395e206b079bbc99cedd895285cc46
Xavi <xavigonzalvo@gmail.com>
2015-05-25 07:57:38 +0100

    Update travis

--- scripts/update_cmake.sh
+++ scripts/update_cmake.sh
@@ -16,11 +16,11 @@ else
     echo
 fi
 
-sudo apt-get update >/dev/null
-sudo apt-get -q install check libprotobuf-c0-dev protobuf-c-compiler
+#sudo apt-get update >/dev/null
+#sudo apt-get -q install check libprotobuf-c0-dev protobuf-c-compiler
 
-#wget https://github.com/google/protobuf/releases/download/v2.5.0/protobuf-2.5.0.tar.gz
-#tar -xvzf protobuf-2.5.0.tar.gz
-#cd protobuf-2.5.0
-#./configure && sudo make install
-#cd ..
+wget https://github.com/google/protobuf/releases/download/v2.5.0/protobuf-2.5.0.tar.gz
+tar -xvzf protobuf-2.5.0.tar.gz
+cd protobuf-2.5.0
+./configure && sudo make install
+cd ..
